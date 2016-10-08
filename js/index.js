64a663d38386ff4bb0ce7d55c06e7895aa067f46
Yurii <yursem@meta.ua>
2016-10-08 17:37:53 +0000

    add new files

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-loaded.innerHTML = (new Date()).toTimeString();
+$(function () {
+    $('#loaded').html((new Date()).toTimeString());
+    $('#makeajax').click(function() {
+      $('#ajaxresult').load('date.html');
+   });
 
-makeajax.addEventListener('click', function(e) {
+/*makeajax.addEventListener('click', function(e) {
     e.preventDefault();
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = fenction() {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             ajaxresult.innerHTML = xhr.responseText;
         }
-    }
-})
+    }*/
+
+});
+    
